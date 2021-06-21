306a9fc2553a2a92205f75c522e40235962c5a1a
liyong <liyong@yit.com>
2021-06-21 20:02:58 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3cbf50e341ff8b906258c1ed71c1d360'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f2d8dcd6-21f7-48c4-b8ee-9bd44b5a075e'
+};
